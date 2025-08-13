eef1d65b83cb233ce683a5f10061735df911d0e1
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-08-13 15:47:02 +0530

    updated the hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,43 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'startup-park';
+export class AppComponent implements  OnInit, OnDestroy {
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  private timerInterval: any;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.startCountdown();
+    }
+  }
+
+  startCountdown() {
+    const launchDate = new Date('2025-09-15T00:00:00').getTime();
+
+    this.timerInterval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = launchDate - now;
+
+      if (distance < 0) {
+        clearInterval(this.timerInterval);
+        return;
+      }
+
+      this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (isPlatformBrowser(this.platformId)) {
+      clearInterval(this.timerInterval);
+    }
+  }
 }
