ab7e5eaeef611a3064cd1b19f4c885718be19722
Salmanulfaris-M <fsalmanul13@gmail.com>
2025-09-08 12:47:17 +0530

    Updated founder section in about page

--- src/app/modules/user/about/about.component.ts
+++ src/app/modules/user/about/about.component.ts
@@ -10,11 +10,6 @@ import { FooterComponent } from '../../../shared/components/user/footer/footer.c
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
- team = [
-    { name: 'Founder One', role: 'CEO & Co-founder', photo: 'https://www.incubenation.com/assets/shafi-B-rhCGUd.jpg' },
-    { name: 'Founder Two', role: 'CTO & Co-founder', photo: 'https://www.pngitem.com/pimgs/m/272-2720656_user-profile-dummy-hd-png-download.png' },
-    { name: 'Mentor One', role: 'Lead Mentor', photo: 'https://www.pngitem.com/pimgs/m/272-2720656_user-profile-dummy-hd-png-download.png' }
-  ];
 
   stats = [
     { value: '200+', label: 'Startups Supported' },
